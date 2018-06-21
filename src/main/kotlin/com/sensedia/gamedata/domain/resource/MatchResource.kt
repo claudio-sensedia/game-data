6240411d990cc705e7a48ba2ecbe8c992bf12be3
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2018-06-20 22:06:42 -0300

    remove jaeger traceid

--- src/main/kotlin/com/sensedia/gamedata/domain/resource/MatchResource.kt
+++ src/main/kotlin/com/sensedia/gamedata/domain/resource/MatchResource.kt
@@ -17,38 +17,18 @@ class MatchResource(private val matchService: MatchService) {
     private val log = LoggerFactory.getLogger(MatchResource::class.java)
 
     @GetMapping
-    fun matches(@RequestHeader("x-request-id", required = false) xreq: String,
-                @RequestHeader("x-b3-traceid", required = false) xtraceid: String,
-                @RequestHeader("x-b3-spanid", required = false) xspanid: String,
-                @RequestHeader("x-b3-parentspanid", required = false) xparentspanid: String,
-                @RequestHeader("x-b3-sampled", required = false) xsampled: String,
-                @RequestHeader("x-b3-flags", required = false) xflags: String,
-                @RequestHeader("x-ot-span-context", required = false) xotspan: String) = this.matchService.matches().doOnNext {
-        log.info("Found all matches. HEADERS: $xreq $xtraceid $xspanid $xparentspanid $xsampled $xflags $xotspan")
+    fun matches() = this.matchService.matches().doOnNext {
+        log.info("Found all matches")
     }
 
     @GetMapping("/{id}")
-    fun get(@PathVariable("id") id: String, @RequestHeader("x-request-id", required = false) xreq: String,
-            @RequestHeader("x-b3-traceid", required = false) xtraceid: String,
-            @RequestHeader("x-b3-spanid", required = false) xspanid: String,
-            @RequestHeader("x-b3-parentspanid", required = false) xparentspanid: String,
-            @RequestHeader("x-b3-sampled", required = false) xsampled: String,
-            @RequestHeader("x-b3-flags", required = false) xflags: String,
-            @RequestHeader("x-ot-span-context", required = false) xotspan: String,
-            @RequestHeader("api-key", required = false) apiKey: String) =
-            this.matchService.get(id).doOnNext {
-                log.info("Found match by ID $id . API-KEY $apiKey  HEADERS: $xreq $xtraceid $xspanid $xparentspanid $xsampled $xflags $xotspan")
-            }
+    fun get(@PathVariable("id") id: String) = this.matchService.get(id).doOnNext {
+        log.info("Found match by ID $id . ")
+    }
 
     @PutMapping("/{id}")
-    fun result(@PathVariable("id") id: String, @RequestBody result: Result, @RequestHeader("x-request-id", required = false) xreq: String,
-               @RequestHeader("x-b3-traceid", required = false) xtraceid: String,
-               @RequestHeader("x-b3-spanid", required = false) xspanid: String,
-               @RequestHeader("x-b3-parentspanid", required = false) xparentspanid: String,
-               @RequestHeader("x-b3-sampled", required = false) xsampled: String,
-               @RequestHeader("x-b3-flags", required = false) xflags: String,
-               @RequestHeader("x-ot-span-context", required = false) xotspan: String) = this.matchService.changeResult(id, result).doOnNext {
-        log.info("Applying match result Match $id Home Team ${result.homeResult} Away Team ${result.awayResult} HEADERS: $xreq $xtraceid $xspanid $xparentspanid $xsampled $xflags $xotspan")
+    fun result(@PathVariable("id") id: String, @RequestBody result: Result) = this.matchService.changeResult(id, result).doOnNext {
+        log.info("Applying match result Match $id Home Team ${result.homeResult} Away Team ${result.awayResult}")
     }
 
     @PutMapping("/{id}/teams")
